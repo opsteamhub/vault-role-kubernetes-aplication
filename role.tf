cd55c1f2254228df610e045f5a41165590e068e3
rafael <rafaelviniciuscardoso@gmail.com>
2022-04-08 19:01:40 -0300

    update role

--- role.tf
+++ role.tf
@@ -5,5 +5,5 @@ resource "vault_kubernetes_auth_backend_role" "role" {
   bound_service_account_names      = [var.bound_service_account_names]
   bound_service_account_namespaces = [each.value]
   token_ttl                        = var.token_ttl
-  token_policies                   = [vault_policy.policy.name]
+  token_policies                   = [vault_policy.policy[0].name]
 }
